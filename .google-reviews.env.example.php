4cd620a5e711cb9537a5432c77613bc5c86175bd
maurop1995-gif <maurop1995@gmail.com>
2026-08-03 16:45:44 -0300

    Agregar archivos principales de Webtrova

---
+++ .google-reviews.env.example.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    // Copiar como /home2/lostrova/.google-reviews.env.php en cPanel.
+    'api_key' => 'REEMPLAZAR_CON_API_KEY',
+    'place_id' => 'ChIJH3wo1T-Bn5URWbjYHGGrkHo',
+    'cache_ttl' => 86400,
+];
